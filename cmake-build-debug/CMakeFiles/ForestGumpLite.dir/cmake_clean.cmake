27b025fdded530dd85a3d9f01992fbe333043d36
Fabrizio <fabrizio.santoro@ens.uvsq.fr>
2018-10-29 12:26:04 +0100

    Supporting Clion now!

---
+++ cmake-build-debug/CMakeFiles/ForestGumpLite.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ForestGumpLite.dir/src/main.cpp.o"
+  "CMakeFiles/ForestGumpLite.dir/src/actors/actor.cpp.o"
+  "ForestGumpLite.pdb"
+  "ForestGumpLite"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ForestGumpLite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
