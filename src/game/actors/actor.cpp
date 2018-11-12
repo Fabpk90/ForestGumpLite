3d7d22d58e748e265aa02e87f79bb178a9078f90
Fabrizio <fabrizio.santoro@ens.uvsq.fr>
2018-11-12 14:36:16 +0100

    HA

--- src/game/actors/actor.cpp
+++ src/game/actors/actor.cpp
@@ -1,25 +1,9 @@
 #include "actor.h"
 
-Actor::Actor(const char* path)
-{
-    tex = new sf::Texture();
-    
-    if(tex->loadFromFile(path))
-    {
-        sprite = new sf::Sprite(*tex);
-        health = 10;
-    }
-    else
-    {
-        delete tex;
-        exit(-1);
-    }
-}
-
 Actor::Actor(const char* path, int health)
 {
     tex = new sf::Texture();
-    
+
     if(tex->loadFromFile(path))
     {
         sprite = new sf::Sprite(*tex);
